fd23cb7200e05460799414cb8972e0d638f2e90d
i513773 <513773@student.fontys.nl>
2024-11-14 12:59:01 +0100

    sidebar in login scherm weggehaald

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import LoginForm from "./components/Auth/LoginForm";
-
 import SignupForm from "./components/auth/SignupForm.jsx";
 import TrainingBuilder from "./pages/TrainingBuilder.jsx";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+    const location = useLocation();
+    const isLoginPage = location.pathname === '/' || location.pathname === '/login';
+
     return (
-        <Router>
         <div className="flex">
-            <Sidebar />
+            {!isLoginPage && <Sidebar />}
             <div className="flex-1">
                 <Routes>
                     <Route path="/" element={<LoginForm />} />
                     <Route path="/login" element={<LoginForm />} />
                     <Route path="/signup" element={<SignupForm />} />
-                    <Route path="/dashboard" element={<Dashboard /> } />
+                    <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/training-builder" element={<TrainingBuilder />} />
                 </Routes>
             </div>
         </div>
-        </Router>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import App from "./App";
 import './index.css';
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 );
